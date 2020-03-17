c0cdfcdabb972a87438d0691f891ee4186ecc714
piotr-kalanski <piotr.kalanski@gmail.com>
2020-03-17 18:08:27 +0100

    t

--- test/test_workflow_configuration.py
+++ test/test_workflow_configuration.py
@@ -1,7 +1,13 @@
 import unittest
+from configuration.workflow_configuration import WorkflowConfiguration
 
 
 class TestWorkflowConfiguration(unittest.TestCase):
+
+    workflow_configuration: WorkflowConfiguration
+
+    def setUp(self):
+        self.workflow_configuration = WorkflowConfiguration()
 
     def test_dummy(self):
         self.assertTrue(True)
