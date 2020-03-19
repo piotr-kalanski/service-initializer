e6da15396e7cf7e41ac1904f158e98f1aaa1cba6
piotr-kalanski <piotr.kalanski@gmail.com>
2020-03-19 16:47:59 +0100

    draft implemention for creating code commit repository

--- src/tasks/aws/codecommit.py
+++ src/tasks/aws/codecommit.py
@@ -1,13 +1,19 @@
 import logging
+import boto3
 from tasks.abstract_task import AbstractTask
 from service_metadata.model import ServiceMetadata
 
-# TODO - implementation - this is only template
+
 class AWS_CodeCommit_CreateRepository_Task(AbstractTask):
     
-#    def __init__(self):
-        
+    def __init__(self):
+        self.client = boto3.client('codecommit')      
 
     def execute(self, service_metadata: ServiceMetadata):
         logging.info("Creating AWS Code Commit repository")
-        # TODO - implementation
+        response = self.client.create_repository(
+            repositoryName=service_metadata.name,
+            repositoryDescription=service_metadata.description
+        )
+        repository_name = response['repositoryName']
+        logging.info(f"AWS Code Commit repository '{repository_name}' created")
