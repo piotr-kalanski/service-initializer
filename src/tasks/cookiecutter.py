155e720179535f2f686bc23979fb42caeb4e96f1
piotr-kalanski <piotr.kalanski@gmail.com>
2020-03-19 19:43:47 +0100

    draft

--- src/tasks/cookiecutter.py
+++ src/tasks/cookiecutter.py
@@ -1,9 +1,9 @@
 import logging
-from cookiecutter.main import cookiecutter
+#from cookiecutter.main import cookiecutter
 from tasks.abstract_task import AbstractTask
 from service_metadata.model import ServiceMetadata
 
-
+# TODO - just draft implementation 
 class Cookiecutter_GenerateProjectDirectory_Task(AbstractTask):
 
     def __init__(self, template_url_field_name: str, parameters_field_name: str, output_dir: str):
@@ -18,11 +18,11 @@ class Cookiecutter_GenerateProjectDirectory_Task(AbstractTask):
         parameters = service_metadata.parameters[self.parameters_field_name]
         output_dir = self.output_dir
         
-        cookiecutter(
-            template=template_url,
-            no_input=True,
-            extra_context=parameters,
-            output_dir=output_dir,
-        )
+        # cookiecutter(
+        #     template=template_url,
+        #     no_input=True,
+        #     extra_context=parameters,
+        #     output_dir=output_dir,
+        # )
 
         logging.info("Generated project directory using cookiecutter")
