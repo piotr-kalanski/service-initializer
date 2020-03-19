244042cc1f07a349e3a6b388c3d4abc8a5d862ae
piotr-kalanski <piotr.kalanski@gmail.com>
2020-03-19 16:58:24 +0100

    don't create boto3 client - it complicates unit tests

--- src/tasks/aws/codecommit.py
+++ src/tasks/aws/codecommit.py
@@ -6,12 +6,12 @@ from service_metadata.model import ServiceMetadata
 
 class AWS_CodeCommit_CreateRepository_Task(AbstractTask):
     
-    def __init__(self):
-        self.client = boto3.client('codecommit')      
+    # def __init__(self):
 
     def execute(self, service_metadata: ServiceMetadata):
         logging.info("Creating AWS Code Commit repository")
-        response = self.client.create_repository(
+        client = boto3.client('codecommit')      
+        response = client.create_repository(
             repositoryName=service_metadata.name,
             repositoryDescription=service_metadata.description
         )
